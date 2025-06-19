18a355bda11e00e4d032ac46408682ba3585ef4b
Christopher Oviedo <coviedon04@gmail.com>
2025-06-19 12:54:56 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { AtletasComponent } from './atletas/atletas.component';
+import { AutenticacionComponent } from './autenticacion/autenticacion.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { InscripcionesComponent } from './inscripciones/inscripciones.component';
+import { PagosComponent } from './pagos/pagos.component';
+import { TorneosComponent } from './torneos/torneos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AtletasComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+    AtletasComponent,
+    AutenticacionComponent,
+    Client
---
+++ src/app/torneos/torneos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-torneos',
+  standalone: false,
+  templateUrl: './torneos.component.html',
+  styleUrl: './torneos.component.css'
+})
+export class TorneosComponent {
+
+}
